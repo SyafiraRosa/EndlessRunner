3a0ae7b33196224efa711178c4fb58e4cfb98b00
SyafiraRosa <firarosa966@gmail.com>
2021-09-15 19:50:57 +0700

    EndlessRunner

---
+++ Assets/Scripts/CharacterSoundController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSoundController : MonoBehaviour
+{
+    public AudioClip jump;
+    public AudioClip gameover;
+
+    private AudioSource audioPlayer;
+
+    public AudioClip scoreHighlight;
+
+    public void PlayScoreHighlight()
+    {
+        audioPlayer.PlayOneShot(scoreHighlight);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioPlayer = GetComponent<AudioSource>();
+    }
+
+    public void PlayJump()
+    {
+        audioPlayer.PlayOneShot(jump);
+    }
+
+    public void PlayGameOver()
+    {
+        audioPlayer.PlayOneShot(gameover);
+            }
+}
